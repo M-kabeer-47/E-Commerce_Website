d21f61f045a40172091fd55520aea744ba617009
Muhammad Kabeer <muhammadkabeer2003@gmail.com>
2024-09-12 15:38:33 +0500

    main

--- client/src/components/HomePage/main.jsx
+++ client/src/components/HomePage/main.jsx
@@ -122,10 +122,10 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router}>
-        <ReactNotification />
-        <ToastContainer />
-      </RouterProvider>
+      <ReactNotification />
+      <RouterProvider router={router} />
+      <ToastContainer />
     </Provider>
   </React.StrictMode>,
 );
+
