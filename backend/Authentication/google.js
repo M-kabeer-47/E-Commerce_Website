c812fa00bef588a9a435d73e2ad3b5b2e3c4966b
Muhammad Kabeer <muhammadkabeer2003@gmail.com>
2024-09-13 15:36:09 +0500

    TESTING

--- backend/Authentication/google.js
+++ backend/Authentication/google.js
@@ -9,7 +9,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:"https://e-commerce-website-phi-vert.vercel.app/auth/google/callback",
+    callbackURL:"hp/auth/google/callback",
     
     
 },async (accessToken,refreshToken,profile,done)=>{
