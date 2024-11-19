2a2affc105cc2b9d01c9c8b58fa6e54216aaef04
Muhammad Kabeer <muhammadkabeer2003@gmail.com>
2024-11-19 21:23:49 +0500

    urkl

--- client/src/components/HomePage/Navbar.jsx
+++ client/src/components/HomePage/Navbar.jsx
@@ -252,7 +252,7 @@ useEffect(()=>{
           
   {(() => {
     if ((token || query.get("token")) && userLoading && !isTokenExpired()) {
-      console.log("token: "+token.substring(0,3)+" query: "+query.get("token").substring(0,4)+" userLoading: "+userLoading+" isTokenExpired: "+isTokenExpired());
+      
       // Display loading skeleton when token exists, user is loading, and token is valid
       return <div className="user-skeleton"></div>;
     } else if ((token || query.get("token")) && !userLoading && !isTokenExpired()) {
