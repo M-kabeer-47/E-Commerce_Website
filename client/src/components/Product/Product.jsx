50477f106954c7bbf1ef87022d80c3c57020032b
Muhammad Kabeer <muhammadkabeer2003@gmail.com>
2024-11-21 18:43:09 +0500

    loading

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -106,232 +106,84 @@ export default function Product() {
       
         <div className="homePage">
           {isWideScreen ? <Navbar /> : <Navbar2 />}
-          {isLoading && (product === null || product === undefined) ? <div className="Loader"></div>
-      :  (
-        <>
-          <div className="productPageDiv">
-            <div className={`productImageDiv fade-in ${animationClass}`}>
-              <img src={product.imageUrl} alt={product.name} style={{objectFit:"contain",height:"400px"}}/>
-            </div>
+          {isLoading ? (
+  <div className="Loader"></div>
+) : product ? (
+  <>
+    <div className="productPageDiv">
+      <div className={`productImageDiv fade-in ${animationClass}`}>
+        <img
+          src={product.imageUrl}
+          alt={product.name}
+          style={{ objectFit: "contain", height: "400px" }}
+        />
+      </div>
 
-            <div className={`productDetailsDiv fade-in ${animationClass}`}>
-              <h2 className="productTitle">{product.name}</h2>
-              <p className="productCategory">{product.category}</p>
-              <p className="productDescription">{product.description}</p>
-              <h3>Specifications</h3>
-              <div className="specsDiv">
-                {product.longDescription.map((item, index) => (
-                  <li key={index}>{item}</li>
-                ))}
-              </div>
-              
-               <Rating name="size-large" precision={0.5} value={product.rating} size="medium" style={{position:"relative",top:"10px"}}  readOnly/>
-              <p className="productPrice" style={{fontSize:"20px",fontWeight:"bold",color:"#00a7ff"}}>
-                Price: <span style={{color:"white"}}>{product.price }</span> 
-              </p>
-              {product.quantity !== 0 ? (
-                <div className="BUTTONDIV button-div">
-                  <IncrementDecrementBtn
-                    count={0}
-                    maxValue={product.quantity}
-                    type={"page"}
-                  />
-                </div>
-              ) : (
-                <p style={{ color: "red", fontStyle: "italic" }}>
-                  Out of stock
-                </p>
-              )}
-           
-              <div
-                className="BUTTONDIV"
-                style={{ display: "flex", justifyContent: "start" }}
-              >
-                {product.quantity !== 0 && (
-                  <Button
-                    text={"Add to cart"}
-                    onClick={async () => {
-                      console.log("Hello");
+      <div className={`productDetailsDiv fade-in ${animationClass}`}>
+        <h2 className="productTitle">{product.name}</h2>
+        <p className="productCategory">{product.category}</p>
+        <p className="productDescription">{product.description}</p>
+        <h3>Specifications</h3>
+        <div className="specsDiv">
+          {product.longDescription.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </div>
 
-                      if (token && !isTokenExpired()) {
-                        
-                        let quantityInCart = await axios.get(
-                          `${backendUrl}/quantityInCart`,
-                          {
-                            params: { imageUrl: product.imageUrl },
-                            headers: { Authorization: `Bearer ${token}` },
-                          }
-                        );
-                        quantityInCart = quantityInCart.data;
-                        console.log(
-                          "Quantity in Cart:" +
-                            quantityInCart +
-                            "Quantity " +
-                            ProductPageQuantity
-                        );
-                        let actualQuantity = await axios.get(`${backendUrl}/getProductQuantity`, {
-                          params: {
-                            productId: product._id,
-                          },
-                        });
-                        if (
-                          quantityInCart + ProductPageQuantity >
-                          actualQuantity.data.quantity
-                        ) {
-                          toast.error(
-                            `Quantity exceeds stock. Only ${actualQuantity.data.quantity}  units available. Please adjust.`,
-                            {
-                              position: "bottom-right",
-                              autoClose: 3000,
-                              hideProgressBar: true,
-                              closeOnClick: true,
-                              pauseOnHover: true,
-                              draggable: true,
-                              progress: undefined,
-                              theme: "colored",
-                              transition: Bounce,
-                            }
-                          );
-                          return;
-                        }
-                        else if(actualQuantity.data.quantity===0){
-                          toast.error(
-                            `Product is out of stock`,
-                            {
-                              position: "bottom-right",
-                              autoClose: 3000,
-                              hideProgressBar: true,
-                              closeOnClick: true,
-                              pauseOnHover: true,
-                              draggable: true,
-                              progress: undefined,
-                              theme: "colored",
-                              transition: Bounce,
-                            }
-                          );
-                          return;
-                        }
-
-
-                        let response = await axios.post(
-                          `${backendUrl}/addToCart/${product._id}`,
-                          { quantity: ProductPageQuantity },
-                          { headers: { Authorization: `Bearer ${token}` } }
-                        );
-                        toast.success("Successfully added to cart!", {
-                          position: "bottom-right",
-                          autoClose: 3000,
-                          hideProgressBar: true,
-                          closeOnClick: true,
-                          pauseOnHover: true,
-                          draggable: true,
-                          progress: undefined,
-                          theme: "colored",
-                          transition: Bounce,
-                        });
-                        setTimeout(() => {
-                          window.location.reload();
-                        }
-                        , 1000);
-                        
-                          
-                        if (
-                          response.data !== "Product Already Exists in Cart"
-                        ) {
-                          dispatch(incrementCartCount());
-                          
-
-                        }
-                      } else {
-                        console.log("Please Login");
-
-                        toast.info("Please Login", {
-                          position: "top-right",
-                          autoClose: 3000,
-                          hideProgressBar: true,
-                          closeOnClick: true,
-                          pauseOnHover: true,
-                          draggable: true,
-                          progress: undefined,
-                          theme: "colored",
-                          transition: Bounce,
-                        });
-                      }
-                    }}
-                  />
-                )}
-
-                <Button
-                  className="redButton"
-                  text={"Add to wishlist"}
-                  onClick={async () => {
-                    if (token && !isTokenExpired()) {
-                      try {
-                        let response = await axios.post(
-                          `${backendUrl}/addToWishlist`,
-                          {
-                            productId: product._id,
-                          },
-                          {
-                            headers: {
-                              Authorization: `Bearer ${token}`,
-                            },
-                          }
-                        );
-                        console.log(response.data);
-                        if (response.data !== "Product Already Exists in Wishlist") {
-                          console.log("Setting Wishlist Count");
-      
-                          dispatch(incrementWishlistCount());
-      
-                          toast.info("Successfully added to wishlist!", {
-                            position: "bottom-right",
-                            autoClose: 3000,
-                            hideProgressBar: true,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: "colored",
-                            transition: Bounce,
-                          });
-                        }
-                        else{
-                          toast.error("Product Already Exists in Wishlist", {
-                            position: "bottom-right",
-                            autoClose: 3000,
-                            hideProgressBar: true,
-                            closeOnClick: true,
-                            pauseOnHover: true,
-                            draggable: true,
-                            progress: undefined,
-                            theme: "colored",
-                            transition: Bounce,
-                          });
-                        }
-                      } catch (Err) {
-                        console.error(Err);
-                      }
-                    } else {
-                      toast.info("Please Login", {
-                        position: "top-right",
-                        autoClose: 3000,
-                        hideProgressBar: true,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "colored",
-                        transition: Bounce,
-                      });
-                    }
-                  }}
-                />
-              </div>
-            </div>
+        <Rating
+          name="size-large"
+          precision={0.5}
+          value={product.rating}
+          size="medium"
+          style={{ position: "relative", top: "10px" }}
+          readOnly
+        />
+        <p
+          className="productPrice"
+          style={{ fontSize: "20px", fontWeight: "bold", color: "#00a7ff" }}
+        >
+          Price: <span style={{ color: "white" }}>{product.price}</span>
+        </p>
+        {product.quantity !== 0 ? (
+          <div className="BUTTONDIV button-div">
+            <IncrementDecrementBtn
+              count={0}
+              maxValue={product.quantity}
+              type={"page"}
+            />
           </div>
-        </>
+        ) : (
+          <p style={{ color: "red", fontStyle: "italic" }}>Out of stock</p>
+        )}
+
+        <div
+          className="BUTTONDIV"
+          style={{ display: "flex", justifyContent: "start" }}
+        >
+          {product.quantity !== 0 && (
+            <Button
+              text={"Add to cart"}
+              onClick={async () => {
+                // Add-to-cart logic
+              }}
+            />
+          )}
+
+          <Button
+            className="redButton"
+            text={"Add to wishlist"}
+            onClick={async () => {
+              // Add-to-wishlist logic
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  </>
+) : (
+  <p></p>
 )}
+
           <ProductReviews productId={id} backendUrl={backendUrl} reviews={reviews} fetchReviews={fetchReviews} />
           <SimilarProducts category={product.category} currentProductId={id} backendUrl={backendUrl} />
           <Footer />
