faf6341ec763a9529da1dde98d5351eaccbfc7a4
Muhammad Kabeer <muhammadkabeer2003@gmail.com>
2024-11-21 09:18:55 +0500

    sdjsd

--- backend/middlewares/multer.js
+++ backend/middlewares/multer.js
@@ -1,13 +1,18 @@
 import multer from 'multer';
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
-// Define the folder path
+// Get the current directory path from the module URL
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Define the folder path for images
 const imagesDir = path.join(__dirname, 'images');
 
 // Check if the images folder exists, and create it if it doesn't
 if (!fs.existsSync(imagesDir)) {
-    fs.mkdirSync(imagesDir, { recursive: true }); // The 'recursive' option ensures that parent directories are also created if they don't exist.
+    fs.mkdirSync(imagesDir, { recursive: true }); // 'recursive: true' ensures parent directories are created if needed.
 }
 
 let filename = "";
